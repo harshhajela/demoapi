a029f5eb6464d3a7325096d8939d5a8b2309d5f5
Harsh Hajela <harsh.hajela@telensa.com>
2020-07-09 16:12:33 +0100

    updated annotation processor with Lombok.

--- src/main/java/com/example/demoapi/dto/SavePlayerDTO.java
+++ src/main/java/com/example/demoapi/dto/SavePlayerDTO.java
@@ -1,6 +1,5 @@
 package com.example.demoapi.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,9 +7,7 @@ import java.io.Serializable;
 @Data
 public class SavePlayerDTO implements Serializable {
 
-    @JsonProperty(value = "username")
     private String username;
 
-    @JsonProperty(value = "password")
     private String password;
 }
